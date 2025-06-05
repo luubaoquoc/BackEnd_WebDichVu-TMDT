01298ff1b223af0cd5ebfb7103b626ddbdf1f59c
Le Trung Son <sonmartin2003@gmail.com>
2025-06-05 17:55:52 +0700

    minor edited OrderModel.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
- async function connect() {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/webdichvu');
-          console.log('Connect successfully!!!');
-    } catch (error) {
-        console.log('Connect failure!!!');
-    }
-  
+async function connect() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://sonmartin2003:Yeyetiger2003@products.eoildal.mongodb.net/WebDichVu_DB"
+    );
+    console.log("Connect successfully!!!");
+  } catch (error) {
+    console.log("Connect failure!!!");
+  }
 }
 
 module.exports = { connect };
